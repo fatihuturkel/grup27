e080a91cef79cfdf7e3e1c14495a473d7ab913e0
Metehan Dündar <bir_kalem049@hotmail.com>
2023-12-30 00:08:50 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         FileReader f1 = new FileReader("src/giris.txt");//dosya yolu verilir. CMDden gelen dosya adı. dist klasörünün içinde olmalı.
         f1.ReadFromFile();//Dosya okuma metodunu �a��r�r.
+        //deneme
     }
 }
 
